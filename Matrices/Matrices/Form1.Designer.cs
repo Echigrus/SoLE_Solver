970d7df52c0901125bbb6a549519332a89488ee5
Echigrus <echigrus@gmail.com>
2019-01-24 03:04:42 +0400

    Add files via upload
    1.0 version

---
+++ Matrices/Matrices/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Matrices
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(222, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(67, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите размер квадратной матрицы:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(376, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Решить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(574, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Определитель:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 22;
+            this.dataGridView1.ShowCellToolTips = false;
+            this.dataGridView1.ShowRowErrors = false;
+            this.dataGridView1.Size = new System.Drawing.Size(358, 348);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(295, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Построить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.ColumnHeadersVisible = false;
+            this.dataGridView2.Location = new System.Drawing.Point(376, 57);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowHeadersWidth = 22;
+            this.dataGridView2.ShowCellToolTips = false;
+            this.dataGridView2.ShowRowErrors = false;
+            this.dataGridView2.Size = new System.Drawing.Size(93, 348);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Коэффициенты";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(574, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(159, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Отформатированная матрица";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.ColumnHeadersVisible = false;
+            this.dataGridView3.Location = new System.Drawing.Point(475, 57);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersVisible = false;
+            this.dataGridView3.RowHeadersWidth = 22;
+            this.dataGridView3.ShowCellToolTips = false;
+            this.dataGridView3.ShowRowErrors = false;
+            this.dataGridView3.Size = new System.Drawing.Size(93, 348);
+            this.dataGridView3.TabIndex = 4;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.AllowUserToAddRows = false;
+            this.dataGridView4.AllowUserToDeleteRows = false;
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.ColumnHeadersVisible = false;
+            this.dataGridView4.Location = new System.Drawing.Point(577, 57);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.ReadOnly = true;
+            this.dataGridView4.RowHeadersVisible = false;
+            this.dataGridView4.RowHeadersWidth = 22;
+            this.dataGridView4.ShowCellToolTips = false;
+            this.dataGridView4.ShowRowErrors = false;
+            this.dataGridView4.Size = new System.Drawing.Size(358, 348);
+            this.dataGridView4.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(700, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Ранг матрицы:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(825, 17);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Решений:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(373, 41);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Неизвестные";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(472, 41);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Чему равно";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(437, 14);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(55, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Гаусс";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(498, 14);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.checkBox2.Size = new System.Drawing.Size(65, 17);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Крамер";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 417);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Решение СЛАУ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Matrices/Matrices/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Matrices
+{
+    public partial class Form1 : Form
+    {
+        static int n, i, j, r1, r2;
+        static double[,] m1, m2, ta;
+        static double[] x, b, det, xc;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void setBtn_Click(object sender, System.EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out n))
+            {
+                n = Convert.ToInt32(textBox1.Text);
+                m1 = new double[n, n];
+                b = new double[n];
+                x = new double[n];
+                m2 = new double[n, n+1];
+                dataGridView1.RowCount = n;
+                dataGridView1.ColumnCount = n;
+                dataGridView2.RowCount = n;
+                dataGridView2.ColumnCount = 1;
+                dataGridView3.RowCount = n;
+                dataGridView3.ColumnCount = 1;
+            }
+        }
+
+        private void solveBtn_Click(object sender, System.EventArgs e)
+        {
+            for (i = 0; i < n; i++)
+            {
+                b[i]= Convert.ToDouble(dataGridView3.Rows[i].Cells[0].Value);
+                m2[i, n] = b[i];
+                for (j = 0; j < n; j++)
+                {
+                    m1[i, j] = Convert.ToDouble(dataGridView1.Rows[i].Cells[j].Value);
+                    m2[i, j] = m1[i, j];
+                }
+            }
+            label2.Text= "Определитель: " + Det(m1);
+            r1 = Rank(n, n, m1); r2 = Rank(n, n+1, m2);
+            label5.Text = "Ранг матрицы: " + r1 + "(" + r2 + ")";
+            if (r1 == r2)
+            {
+                if (r1 == n)
+                {
+                    label6.Text = "Решений: одно";
+                    if (checkBox1.Checked)
+                    {
+                        FormatSystem(n, n+1, m2);
+                        x= GaussMethod(m2);
+                        for (i = 0; i < n; ++i) dataGridView2.Rows[i].Cells[0].Value = x[i];
+                    }
+                    else
+                    {
+                        dataGridView4.RowCount = n;
+                        dataGridView4.ColumnCount = 2;
+                        x = Cramer(m1, n, b);
+                        for (i = 0; i < n; ++i) dataGridView2.Rows[i].Cells[0].Value = x[i];
+                    }
+                }
+                if(r1<n) 
+                {
+                    label6.Text = "Решений: ∞";
+                    FormatSystem(n, n+1, m2);
+                }
+            }
+            else label6.Text = "Решений: нет";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Click += new EventHandler(this.setBtn_Click);
+            button1.Click += new EventHandler(this.solveBtn_Click);
+            checkBox1.Click += new EventHandler(this.ch1_Click);
+            checkBox2.Click += new EventHandler(this.ch2_Click);
+        }
+
+        public void ch1_Click(object sender, System.EventArgs e)
+        {
+            checkBox2.Checked = false;
+        }
+        public void ch2_Click(object sender, System.EventArgs e)
+        {
+            checkBox1.Checked = false;
+        }
+
+        public double[] Cramer(double[,] a, int size, double[] b) {
+            int i, k, j;
+            det = new double[size + 1]; xc = new double[size];
+            det[0] = Det(a);
+            ta = new double[size, size];
+            for (i = 0; i < size; i++)
+            {
+                for (j = 0; j < size; j++)
+                {
+                    for (k = 0; k < size; k++) {
+                        ta[j, k] = a[j, k];
+                    }
+                }
+                for (k = 0; k < size; k++)
+                {
+                    ta[k, i] = b[k];
+                }
+                det[i + 1] = Det(ta);
+                dataGridView4.Rows[i].Cells[0].Value = "det"+(i+1);
+                dataGridView4.Rows[i].Cells[1].Value = det[i + 1];
+                xc[i] = det[i + 1] / det[0];
+                
+            }
+            return xc;
+        }
+
+        public static double Det(double[,] a)
+        {
+            int m = a.GetLength(0), p = a.GetLength(1), idet;
+            double d = 0;
+            if (m == p)
+            {
+                if (m > 2)
+                {
+                    for (idet = 0; idet < m; idet++) {
+                        if(idet%2==1) d = d - a[0, idet] * Pivot(a, 0, idet);
+                        else d = d + a[0, idet] * Pivot(a, 0, idet);
+                    }
+                    return d;
+                }
+                else if (m == 2) return a[0, 0] * a[1, 1] - a[0, 1] * a[1, 0];
+                else if (m == 1) return a[0, 0];
+                else return 0;
+            }
+            else return 0;
+        }
+
+        public static double Pivot(double[,] a, int row, int col) {
+            int m = a.GetLength(0), ip, jp;
+            double[,] tp = new double[m-1, m-1];            
+            for (ip = 0; ip < m; ip++) {
+                for (jp = 0; jp < m; jp++) {
+                    if (ip < row && jp < col) tp[ip, jp] = a[ip, jp];
+                    else if (ip > row && jp < col) tp[ip - 1, jp] = a[ip, jp];
+                    else if (ip < row && jp > col) tp[ip, jp - 1] = a[ip, jp];
+                    else if (ip > row && jp > col) tp[ip - 1, jp - 1] = a[ip, jp];
+                }
+            }
+
+            return Det(tp);
+        }
+
+        void FormatSystem(int nf, int mf, double[,] a)
+        {
+            int i, j, k;
+            double E, temp;
+            double[] tempm = new double[mf];
+            for (k = 0; k < nf; k++) //перебор строк
+            {
+                if (a[k, k] != 0) //главная диагональ
+                {
+                    for (i = k + 1; i < nf; i++) //от k+1 до конца
+                    {
+                        temp = a[k, k];
+                        for (j = 0; j < mf; j++) { a[k, j] /= temp; } //делит строку k на a[k][k]
+                        E = -a[i, k] / a[k, k]; //делитель
+                        for (j = k; j < mf; j++)
+                        {
+                            a[i, j] += E * a[k, j]; //все строки вниз умножаются на делитель
+                        }
+                    }
+                }
+                else
+                {
+                    for (i = 0; i < k; i++)
+                    {
+                        if (i > k && a[i, k] != 0)
+                        {
+                            for (j = 0; j < mf; j++) tempm[j] = a[k, j];
+                            for (j = 0; j < mf; j++) a[k, j] = a[i, j];
+                            for (j = 0; j < mf; j++) a[i, j] = tempm[j];
+                        }
+                    }
+                }
+            }
+
+            dataGridView4.RowCount = nf;
+            dataGridView4.ColumnCount = mf;
+            for (k = 0; k < nf; ++k)
+                for (i = 0; i < mf; ++i) dataGridView4.Rows[k].Cells[i].Value = a[k,i];
+               
+
+        }
+        public static double[] GaussMethod(double[,] a)
+        {
+            int m = a.GetUpperBound(0);//ширина
+            int n = a.GetUpperBound(1);//высота
+            int i = 0, j;
+            double[] x;
+            x = new double[n];
+
+            for (i = n - 1; i >= 0; --i)
+            {
+                x[i] = a[i, n] / a[i, i];
+                for (j = i + 1; j < n; ++j)
+                {
+                    x[i] -= x[j] * (a[i, j] / a[i, i]);
+                }
+            }
+
+            return x;
+        }
+
+        public int Rank(int nr, int mr, double[,] a)
+        {
+            int iz, jz, kz, rank=0;
+            double E, tr;
+            double[] tempm = new double[mr];
+            Boolean cleanLine = false;
+            double[,] ra = new double[nr, mr];
+            for (kz = 0; kz < nr; kz++)
+                for (iz = 0; iz < mr; iz++) ra[kz, iz] = a[kz, iz];
+            for (kz = 0; kz < nr; kz++) //вертикаль
+            {
+                if (ra[kz, kz] != 0) //главная диагональ
+                {
+                    for (iz = kz + 1; iz < nr; iz++) //от k+1 до конца
+                    {
+                        tr = ra[kz, kz];
+                        for (jz = 0; jz < mr; jz++) ra[kz, jz] /= tr; //делит строку k на a[k][k]
+                        E = -ra[iz, kz] / ra[kz, kz]; //делитель
+                        for (jz = kz; jz < mr; jz++)
+                        {
+                            ra[iz, jz] += E * ra[kz, jz]; //все строки вниз умножаются на делитель
+                        }
+                    }
+                }
+                else
+                {
+                    for (iz = 0; iz < kz; iz++)
+                    {
+                        if (iz > kz && ra[iz, kz] != 0)
+                        {
+                            for (jz = 0; jz < mr; jz++) tempm[jz] = ra[kz, jz];
+                            for (jz = 0; jz < mr; jz++) ra[kz, jz] = ra[iz, jz];
+                            for (jz = 0; jz < mr; jz++) ra[iz, jz] = tempm[jz];
+                        }
+                    }
+                }
+            }
+            for (iz = 0; iz < nr; iz++)
+            {
+                cleanLine = false;
+                for (jz = 0; jz < mr; jz++)
+                {
+                    if (ra[iz, jz] != 0) cleanLine = true;
+                }
+                if (cleanLine) rank++;
+            }
+            return rank;
+
+        }
+    }
+}
